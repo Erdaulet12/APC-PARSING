eed1f082f4537705d83f5a2946c24db1215b4ea0
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-08-26 21:04:11 +0500

    main

---
+++ main.py
@@ -0,0 +1,93 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://apc.kz/'
+visited_urls = set()
+
+
+def get_soup(url):
+	response = requests.get(url)
+	return BeautifulSoup(response.text, 'html.parser')
+
+
+def get_categories(soup):
+	categories = []
+	for category in soup.select('.col-lg-2.col-md-3.col-sm-4.col-xs-6'):
+		category_name = category.select_one('h4 a').text.strip()
+		category_link = category.select_one('h4 a')['href']
+		if category_link not in visited_urls:
+			visited_urls.add(category_link)
+			categories.append((category_name, category_link))
+	return categories
+
+
+def get_subcategories(category_url):
+	soup = get_soup(category_url)
+
+	subcategories = []
+	for subcategory in soup.select('.refine_categories a'):
+		subcategory_name = subcategory.select_one('span').text.strip()
+		subcategory_link = subcategory['href']
+		if subcategory_link not in visited_urls:
+			visited_urls.add(subcategory_link)
+			subcategories.append((subcategory_name, subcategory_link))
+
+	if not subcategories:
+		print(f'  В категории нет подкатегорий, ищем товары...')
+		products = get_products(category_url)
+		return products, subcategories
+
+	return [], subcategories
+
+
+def get_products(subcategory_url):
+	soup = get_soup(subcategory_url)
+	products = []
+	for product in soup.select('.product-layout'):
+		product_name = product.select_one('.caption h4 a span').text.strip()
+		product_link = product.select_one('.caption h4 a')['href']
+		if product_link not in visited_urls:  # Проверяем, был ли URL уже посещён
+			visited_urls.add(product_link)
+			products.append((product_name, product_link))
+	return products
+
+
+def get_product_details(product_url):
+	soup = get_soup(product_url)
+	details = {}
+
+	for row in soup.select('tr[itemprop="additionalProperty"]'):
+		name = row.select_one('td[itemprop="name"]').text.strip()
+		value = row.select_one('td[itemprop="value"]').text.strip()
+		details[name] = value
+
+	return details
+
+
+def main():
+	soup = get_soup(URL)
+	categories = get_categories(soup)
+
+	for category_name, category_link in categories:
+		print(f'Категория: {category_name}')
+		products, subcategories = get_subcategories(category_link)
+
+		if not subcategories:
+			print(f'  В категории "{category_name}" нет подкатегорий, переходим к товарам...')
+			products = get_products(category_link)
+			for product_name, product_link in products:
+				print(f'    Товар: {product_name}')
+				details = get_product_details(product_link)
+				print(f'      Характеристики: {details}')
+		else:
+			for subcategory_name, subcategory_link in subcategories:
+				print(f'  Подкатегория: {subcategory_name}')
+				products = get_products(subcategory_link)
+				for product_name, product_link in products:
+					print(f'    Товар: {product_name}')
+					details = get_product_details(product_link)
+					print(f'      Характеристики: {details}')
+
+
+if __name__ == '__main__':
+	main()
